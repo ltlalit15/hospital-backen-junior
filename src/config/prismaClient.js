2cc536d144f9d5ee812000523386f64836f3e1cb
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-11-18 17:47:08 +0530

    Update prismaClient.js

--- src/config/prismaClient.js
+++ src/config/prismaClient.js
@@ -1,14 +1,6 @@
 // src/config/prismaClient.js
-// src/config/prismaClient.js
 import { PrismaClient } from "@prisma/client";
 
-// Add this check to ensure DATABASE_URL is available
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is not defined");
-}
-
-const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-});
+const prisma = new PrismaClient();
 
 export default prisma;
